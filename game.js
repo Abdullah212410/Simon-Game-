b72ff971e8ffb7ee864d45e61acc52843595a881
Abdullah Abu Saleem <140598649+Abdullah212410@users.noreply.github.com>
2025-09-30 21:48:14 +0300

    Add files via upload

---
+++ game.js
@@ -0,0 +1,81 @@
+let buttonColours = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+let userClickedPattern = [];
+let level = 0;
+var started = false;
+// function number 1
+function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("level " + level);
+
+  let randomNumber = Math.floor(Math.random() * 4);
+  let randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+  $("#" + randomChosenColour)
+    .fadeOut(100)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+  playSound(randomChosenColour);
+  animatePress(randomChosenColour);
+}
+//
+$(".btn").click(function handler() {
+  if (!started) {
+    nextSequence();
+    started = true;
+  }
+  let userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  console.log(userClickedPattern);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length - 1);
+});
+//
+function playSound(name) {
+  let audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+//
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+}
+//
+$(document).keypress(function (event) {
+  if (!started) {
+    nextSequence();
+    started = true;
+  }
+});
+//
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("success");
+    if (gamePattern.length === userClickedPattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    console.log("wrong");
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+    $("h1").text("Game Over, Press Any Key to Restart");
+
+    startOver();
+  }
+}
+//
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
